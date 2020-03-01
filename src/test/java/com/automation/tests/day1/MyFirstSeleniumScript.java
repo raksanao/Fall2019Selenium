e8a2c1b6774325cba0cef4a0768806eabea725c9
Rukhshona Olimova <rukhshona87@gmail.com>
2020-03-01 11:32:53 -0600

    first commit

---
+++ src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
@@ -0,0 +1,12 @@
+package com.automation.tests.day1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstSeleniumScript {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver drive= new ChromeDriver();
+        drive.get("http://google.com");
+    }
+}
