1c3a8be6157c31cb43e81758708de97ed63f7c76
Rukhshona Olimova <rukhshona87@gmail.com>
2020-03-01 12:53:54 -0600

    commit

--- src/test/java/day2/BasicNavigation.java
+++ src/test/java/day2/BasicNavigation.java
@@ -7,12 +7,22 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         //ChromeDriver driver=new ChromeDriver();
        // RemoteWebDriver dr=new SafariDriver();
-        WebDriver drive=new ChromeDriver();
-        drive.get("http://google.com");
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://google.com");
+        Thread.sleep(3000);
+        String title=driver.getTitle();
+        String expectedTitle="Google";
+        System.out.println("Title is "+title);
+        if(expectedTitle.equals("Google")){
+            System.out.println("Test passed");
+        }else{
+            System.out.println("Failed");
+        }
+        driver.close();
     }
 
 }
