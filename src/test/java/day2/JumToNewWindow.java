71a4cca876a4bec8c8ceb5c4d71b28ebc59e9685
Rukhshona Olimova <rukhshona87@gmail.com>
2020-03-01 15:55:05 -0600

    commit

---
+++ src/test/java/day2/JumToNewWindow.java
@@ -0,0 +1,42 @@
+package day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Set;
+
+public class JumToNewWindow {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/open_new_tab");
+        Thread.sleep(5000);
+        String windowHandle=driver.getWindowHandle();
+        System.out.println(windowHandle);
+        Set<String> windowHandles=driver.getWindowHandles();
+
+        System.out.println(windowHandles);
+        System.out.println("Before: "+driver.getCurrentUrl());
+
+      for(String windowId:windowHandles){
+          if(!windowId.equals(windowHandle)){
+              driver.switchTo().window(windowId);
+          }
+      }
+        System.out.println("After switch "+driver.getCurrentUrl());
+      driver.close();
+    }
+    public static void switchToWindorBasedOnTitel(String pageTitle,WebDriver driver){
+
+        Set<String>windows=driver.getWindowHandles();
+        for(String window:windows){
+            driver.switchTo().window(window);
+            if(driver.getTitle().equals(pageTitle)){
+                break;
+            }
+        }
+    }
+}
--- src/test/java/day2/QuiteVsClose.java
+++ src/test/java/day2/QuiteVsClose.java
@@ -1,7 +1,17 @@
 package day2;
 
-public class QuiteVsClose {
-    public static void main(String[] args) {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class QuiteVsClose {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/open_new_tab");
+        Thread.sleep(4000);
+       driver.quit();
+
+       // driver.close();
     }
 }
